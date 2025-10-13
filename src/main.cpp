229bb5a6f7a9fe8fe9d599c4e5a420c8f5e46f3f
R0-main <justromixx@gmail.com>
2025-10-13 14:05:25 +0200

    changed log and clean warning log

--- src/main.cpp
+++ src/main.cpp
@@ -82,8 +82,8 @@ int main(int argc, char **argv)
         return 1;
     }
 
-    Logger::setLevel(DEBUG);
-    // Logger::setLevel(INFO);
+    // Logger::setLevel(DEBUG);
+    Logger::setLevel(INFO);
     
     server.run();
     
